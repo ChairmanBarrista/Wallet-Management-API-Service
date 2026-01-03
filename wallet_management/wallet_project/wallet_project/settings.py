4f263d01cc6af7581805000dc46eb40a68603987
Emmanuel Ofosu Amoah <barristaamoah@gmail.com>
2026-01-03 23:27:30 +0000

    New project folder for my Capstone Project

--- wallet_management/wallet_project/wallet_project/settings.py
+++ wallet_management/wallet_project/wallet_project/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#(e(9s-%r=6ns$#g(wyr%*bbjtzdmbn%hcqh6=781vvofrvyxo'
+SECRET_KEY = 'django-insecure-126l6bgw6qn8ws%!f55@2p5fuv5kal8^^rd2-4(o^j&bi*46jf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -37,10 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework.authtoken',
-    'corsheaders',
-    'rest_framework',
-    'wallets',
 ]
 
 MIDDLEWARE = [
@@ -51,7 +47,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',  # Add CORS
 ]
 
 ROOT_URLCONF = 'wallet_project.urls'
@@ -77,45 +72,13 @@ WSGI_APPLICATION = 'wallet_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
-# MySQL Database Configuration
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'wallet_db',
-        'USER': 'root',
-        'PASSWORD': 'Kuuza20020929',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
-# REST Framework Configuration
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
-}
-
-# CORS Settings (if needed for frontend)
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
--- wallet_management/wallet_project/wallet_project/wsgi.py
+++ wallet_management/wallet_project/wallet_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for wallet_management project.
+WSGI config for wallet_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wallet_management.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wallet_project.settings')
 
 application = get_wsgi_application()
