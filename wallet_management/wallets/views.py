a8532a20fc34f96284c2ff9149c2d86d972a9cab
Emmanuel Ofosu Amoah <barristaamoah@gmail.com>
2026-01-03 00:24:42 +0000

    Views using generic class-based views for efficiency.

--- wallet_management/wallets/views.py
+++ wallet_management/wallets/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Wallet
+from .serializers import WalletSerializer
 
-# Create your views here.
+class WalletCreateView(generics.CreateAPIView):
+    serializer_class = WalletSerializer
+    permission_classes = [IsAuthenticated]
+
+class WalletListView(generics.ListAPIView):
+    serializer_class = WalletSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Wallet.objects.filter(user=self.request.user)
+
+class WalletDetailView(generics.RetrieveAPIView):
+    serializer_class = WalletSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Wallet.objects.filter(user=self.request.user)
+
+class WalletDeleteView(generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Wallet.objects.filter(user=self.request.user)
