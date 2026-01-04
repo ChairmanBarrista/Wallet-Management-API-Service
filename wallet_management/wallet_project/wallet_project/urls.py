619af60938505a7b7cfdb1ac608d5c88a6b436ba
Emmanuel Ofosu Amoah <barristaamoah@gmail.com>
2026-01-04 21:43:29 +0000

    wallet_project urls

--- wallet_management/wallet_project/wallet_project/urls.py
+++ wallet_management/wallet_project/wallet_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for wallet_project project.
+URL configuration for wallet_api project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -14,14 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# wallet_project/urls.py
-
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework.authtoken.views import obtain_auth_token
+from django.urls import path , include
+
+#urlpatterns = [
+#    path('admin/', admin.site.urls),
+#]
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
     path('api/', include('wallets.urls')),
-    path('api/auth/login/', obtain_auth_token, name='api_token_auth'),
 ]
